22278f53b8840aac2c95ac36e6f60c8040acf591
hubertTCM <fuqunxing@gmail.com>
2014-01-05 21:56:31 +0800

    upload major part to parse consilia for fzl

--- dataImporter/ConsiliaProvider/provider_fzl.py
+++ dataImporter/ConsiliaProvider/provider_fzl.py
@@ -11,19 +11,76 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 def appendAncestorsToSystemPath(levels):
-	parentFolder = os.path.dirname(__file__)
+	parent = os.path.dirname(__file__)
 	for i in range(levels):
-		sys.path.append(parentFolder)
-		parentFolder = os.path.abspath(os.path.join(parentFolder,".."))
+		sys.path.append(parent)
+		parent = os.path.abspath(os.path.join(parent, ".."))
 		
 appendAncestorsToSystemPath(3)
 
 from dataImporter.Utils.Utility import *
 
-class Provider_fzl:
-	def __createConsilias(self, content):
-		pass
+class Provider_fzl:	
+	def __exact_detail_situation(self, sourceText, targetDictionary):
+		diagnosis_keywords =[u'处方：', u'处方一：', u'处方: ']
+		index = -1
+		for keyword in diagnosis_keywords:
+			index = sourceText.find(keyword)
+			if(index >= 0):
+				targetDictionary['description'] = sourceText[:index]
+				targetDictionary['diagnosis'] = sourceText[index:]
+				print 'description:  ' + targetDictionary['description']
+				print 'diagnosis:  ' + targetDictionary['diagnosis']
+				break
+		
+		if (index < 0):
+			targetDictionary['diagnosis'] = sourceText
+			print 'diagnosis:  ' + targetDictionary['diagnosis']
+			
+	def __exact_detail(self, whichTime, sourceText):
+		comment_keywords = (u'［按语］', u'［辨证］')
+		detail = {}
+		for keyword in comment_keywords:
+			index = sourceText.find(keyword)
+			if(index >= 0):
+				self.__exact_detail_situation(sourceText[:index], detail)
+				detail['comments'] = sourceText[index:]
+				print 'comment:  ' + detail['comments']				
+				return detail
+			
+		self.__exact_detail_situation(sourceText, detail)		
+		return detail
+		
+	def _createAllDetails(self, which_time, sourceText, targetDetails):
+		index = sourceText.find(u'诊］', 3)
+		if (index > 0):
+			self.__exact_detail(which_time, sourceText[:index - 2].strip())
+			self._createAllDetails(which_time + 1, sourceText[index - 2:].strip(), targetDetails)
+		else:
+			detailItem = self.__exact_detail(which_time, sourceText)
+			targetDetails.append((which_time, detailItem))
 	
+	def __createConsilia(self, title, content):
+		content = content[content.index(title) + len(title):].strip()
+		keywords = (u'［初诊］', u'［一诊］', u'［诊治］')
+		
+		consilia = {}
+	
+		for keyword in keywords:
+			index = content.find(keyword)
+			if(index >= 0):
+				consilia['description'] = content[:index]
+				content = content[index:]
+				print 'description:  ' + consilia['description'] 				
+				break
+
+		details = []
+		self._createAllDetails(1, content.strip(), details)
+		consilia['details'] = details
+		return consilia
+	
+	def _exactTitleInformation(self, sourceText):
+		pass
 	def getAllConsilias(self):			
 		file = codecs.open('fzl.txt', 'r', 'utf-8', 'ignore')
 		content = file.read()
@@ -33,136 +90,22 @@ class Provider_fzl:
 		for i in range(len(matches)):
 			itemstart = matches[i]
 			if i == len(matches) - 1:
-				'''last item'''
-				item = content[content.index(itemstart):]
+				'''last sourceText'''
+				sourceText = content[content.index(itemstart):]
 			else:
-				item = content[content.index(itemstart):content.index(matches[i+1])]
-			item = item.strip()
+				sourceText = content[content.index(itemstart):content.index(matches[i+1])]
+			sourceText = sourceText.strip()
 			#print itemstart
 			#return
 			index = itemstart.find(u'、') + 1
 			title = itemstart[index:]
 			#title = itemstart.split(u'、')[1].strip()
 			print title	
-			consilia = {'comeFrom': {'category': u'Book', 'Name': u'范中林六经辨证医案'}, 'Author': u'范中林'}
-			
-			
+			consilia = {'comeFrom': {'category': u'Book', 'Name': u'范中林六经辨证医案'}, 'Author': u'范中林'}	
+			self._exactTitleInformation(title)		
+			Utility.update_dict(consilia, self.__createConsilia(title, sourceText))
+			#yield consilia			
 
 p = Provider_fzl()
 p.getAllConsilias()
 
-
-def exact_detail_situation(detail, item):
-	diagnosis_keywords =[u'处方：', u'处方一：', u'处方: ']
-	index = -1
-	for keyword in diagnosis_keywords:
-		index = item.find(keyword)
-		if(index >= 0):
-			detail.description = item[:index]
-			detail.diagnosis = item[index:]
-			break
-	
-	if (index < 0):
-		detail.diagnosis = item
-
-def exact_detail(summary, which_time, item):
-	comment_keywords = (u'［按语］', u'［辨证］')
-	detail = YiAnDetail()
-	detail.index = which_time
-	detail.yian = summary
-	
-	for keyword in comment_keywords:
-		index = item.find(keyword)
-		if(index >= 0):
-			exact_detail_situation(detail, item[:index])
-			detail.comments = item[index:]
-			
-			print detail.description
-			print "***"
-			print detail.diagnosis
-			print "***"
-			print detail.comments
-			print "###"
-			
-			detail.save()
-			return
-	
-	exact_detail_situation(detail, item)
-	detail.save()
-	print detail.description
-	print "***"
-	print detail.diagnosis
-	print "###"
-	
-def exact_detail_list(summary, which_time, item):
-	index = item.find(u'诊］', 3)
-	if (index > 0):
-		exact_detail(summary, which_time, item[:index - 2].strip())
-		exact_detail_list(summary, which_time + 1, item[index - 2:].strip())
-	else:
-		exact_detail(summary, which_time, item)
-
-def exact(doctor, title, item):
-	summary = YiAnSummary()
-	summary.author = doctor
-	summary.title = title
-
-	item = item[item.index(title) + len(title):].strip()
-	keywords = (u'［初诊］', u'［一诊］', u'［诊治］')
-
-	for keyword in keywords:
-		index = item.find(keyword)
-		if(index >= 0):
-			summary.description = item[:index]
-			item = item[index:]
-			break
-	print summary.description
-	print "***"
-	summary.sourceCategory = u'Book'
-	summary.sourceDetail = u'范中林六经辨证医案'
-	summary.save()
-	return exact_detail_list(summary, 1, item.strip())
-	
-	tlist = item.split(u'［')
-	
-"""
-	tlist = item.split(u'［')
-	if tlist[2].find(u'此') > 0:
-		temp_index = tlist[2].index(u'此')
-		split_index = len(tlist[0]) + len(tlist[1]) + temp_index + 2
-	elif tlist[2].find(u'证') > 0:
-		temp_index = tlist[2].index(u'证')
-		split_index = len(tlist[0]) + len(tlist[1]) + temp_index + 2
-	else:
-		temp_index = tlist[1].index(u'此')
-		split_index = len(tlist[0]) + temp_index + 1
-	situation = item[item.index(title) + len(title):split_index]
-	diagnosis = item[split_index:]	
-"""
-
-def import_fzl():
-	doctor, is_created = TCMDoctor.objects.get_or_create(name=u'范中林')
-	if (is_created):
-		doctor.save()
-		
-	file = codecs.open('fzl.txt', 'r', 'gbk', 'ignore')
-	content = file.read()
-	file.close()
-
-	matches = re.findall(ur"(\d{1,2}\u3001.+)", content, re.M)
-	for i in range(len(matches)):
-		itemstart = matches[i]
-		if i == len(matches) - 1:
-			'''last item'''
-			item = content[content.index(itemstart):]
-		else:
-			item = content[content.index(itemstart):content.index(matches[i+1])]
-		item = item.strip()
-		#print itemstart
-		#return
-		index = itemstart.find(u'、') + 1
-		title = itemstart[index:]
-		#title = itemstart.split(u'、')[1].strip()
-		print title
-		print '############'		
-		exact(doctor, title, item)

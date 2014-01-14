6b1c4131128492a157e4d6ba995d5579b77efb3a
hubertTCM <fuqunxing@gmail.com>
2014-01-14 15:41:07 +0800

    recommit missed files

--- dataImporter/Utils/Utility.py
+++ dataImporter/Utils/Utility.py
@@ -1,20 +1,56 @@
 
 class Utility(object):
+    def get_value(key, dictionary, default_value = None):
+        if (dictionary is None):
+            return default_value
+              
+        if (key in dictionary):
+            return dictionary[key]
+        return default_value
+    
+    get_value = staticmethod(get_value)  
+    
+    def get_bool_value(key, dictionary):
+        value = Utility.get_value(key, dictionary, 'False')
+        return value == 'True'
+    
+    get_bool_value = staticmethod(get_bool_value)
+
     def update_dict(dest, source):
         if (source is None or dest is None):
             return 
         dest.update(source)
+        
     update_dict = staticmethod(update_dict)
+    
+    def update_dictionary_if_not_none(key, value, dictionary):
+        if (value is not None):
+            dictionary[key] = value
+            return True
+        return False
+    
+    update_dictionary_if_not_none = staticmethod(update_dictionary_if_not_none)
+    
+    def append_if_not_none(value, target_list):
+        if (value is not None):
+            target_list.append(value)
+            return True
+        return False
+    
+    append_if_not_none = staticmethod(append_if_not_none)
     
     def run_action_when_key_exists(key, dictionary, action):
         if (key in dictionary):
-            action(dictionary[key])
+            return action(dictionary[key])
+        return None
+    
     run_action_when_key_exists = staticmethod(run_action_when_key_exists)
     
     def apply_default_if_not_exist(dest, default):
         for key, value in default.items():
             if (not key in dest):
                 dest[key] = value
+                
     apply_default_if_not_exist = staticmethod(apply_default_if_not_exist)
             
     
---
+++ dataImporter/Utils/WebUtil.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import re
+from lxml import etree
+from StringIO import StringIO
+from urllib import urlopen
+
+reload(sys)
+#sys.setdefaultencoding('utf-8')
+sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"..")))
+
+from Utility import *
+  
+
+class web_extractor(object):
+    def remove_redundant_space(source):
+        content = source.replace('&nbsp', u' ')
+        content = re.sub(' +', ' ', source)
+        content = re.sub('\n +', '\n', content)
+        content = re.sub(u'\n+', u'\n', content)
+        return content
+    
+    remove_redundant_space = staticmethod(remove_redundant_space)
+
+    def get_content_from(url):
+        item = urlopen(url)
+        content = item.read()
+        blank_items = [u'\xa0', '&nbsp;', '&nbsp']
+        for blank_item in blank_items:
+            content = content.replace(blank_item, u' ')
+        item.close()
+        return web_extractor.remove_redundant_space(content)
+    
+    get_content_from = staticmethod(get_content_from)
+
+    def get_full_text(element):
+        items = [element.prefix, element.text, element.tail]
+        return ''.join([item for item in items if item is not None])
+    
+    get_full_text = staticmethod(get_full_text)
+        
+    def get_text(element, include_text_from_descendant):
+        if (not include_text_from_descendant):
+            return element.text
+        return '\n'.join([web_extractor.get_full_text(descendant) for descendant in element.iter()])
+    
+    get_text = staticmethod(get_text)            
+
+    def extract_single_attribute(element, attri_info):
+        source_attri_name = Utility.get_value('source_attri', attri_info)
+        target_attri_name = Utility.get_value('target_attri_name', attri_info, source_attri_name)
+        
+        if (target_attri_name is None):
+            return None
+        
+        attribute_value = None
+        if (source_attri_name is not None):            
+            attribute_value = element.get(source_attri_name)
+        else:
+            include_text_from_descendant = Utility.get_bool_value('include_text_from_descendant', attri_info)
+            attribute_value = web_extractor.get_text(element, include_text_from_descendant)
+                
+        if (target_attri_name is None or attribute_value is None):
+            return None
+        
+        print attribute_value
+        return {target_attri_name : attribute_value}
+        
+    extract_single_attribute = staticmethod(extract_single_attribute)    
+        
+    def extract_attributes(element, attributes):
+        attribute_values = {}
+        for attri_info in attributes:
+            from_element = element
+            xpath_value = Utility.get_value('xpath', attri_info)
+            if (xpath_value is not None):
+                children = element.xpath(xpath_value)    
+                if children is None or len(children) != 1:
+                    continue    
+                from_element = children[0]
+
+            single_value = web_extractor.extract_single_attribute(from_element, attri_info)    
+            if (single_value is not None):
+                attribute_values.update(single_value)
+                
+        return attribute_values
+
+    extract_attributes = staticmethod(extract_attributes)    
+    
+    def extract_children(element, children_config):
+        if (children_config is None or element is None):
+            return None
+            
+        all_properties = []
+        for prop in children_config:
+            item = web_extractor.get_values_from_html_tree(element, prop)
+            Utility.append_if_not_none(item, all_properties)
+        
+        if len(all_properties) > 0:
+            return all_properties
+        return None
+        
+    extract_children = staticmethod(extract_children)
+    
+    def get_values_from_html_tree(root, config):
+        try:            
+            xpath_name = 'xpath'
+            if (xpath_name in config):
+                root = root.xpath(config[xpath_name])        
+            
+            children_key = 'children' 
+            children_config = Utility.get_value(children_key, config)
+            all_values = []
+            for element in root:
+                value_item = {}
+                children_value = web_extractor.extract_children(element, children_config)
+                Utility.update_dictionary_if_not_none(children_key, children_value, value_item)
+                
+                attributes_key = 'extract_attributes'
+                if (attributes_key in config):
+                    attributes_value = web_extractor.extract_attributes(element, config[attributes_key])
+                    Utility.update_dictionary_if_not_none(attributes_key, attributes_value, value_item)
+                    
+                all_values.append(value_item)
+                
+            if len(all_values) > 0:
+                return all_values
+        except Exception,ex:
+            print Exception,":",ex
+        
+        return None
+            
+    get_values_from_html_tree = staticmethod(get_values_from_html_tree)
+    
+    def get_html_root(url):
+        content = web_extractor.get_content_from(url)
+        parser = etree.HTMLParser()
+        html_tree = etree.parse(StringIO(content), parser)
+        root = html_tree.getroot()
+        return root
+    
+    get_html_root = staticmethod(get_html_root)        
+        
+    def get_values_from_url(url, config):
+        root = web_extractor.get_html_root(url)
+        items = web_extractor.get_values_from_html_tree(root, config)
+        return items 
+           
+    get_values_from_url = staticmethod(get_values_from_url)    

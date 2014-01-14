18d9ca25ffc718d4b6908e6c2f3be02fa120d01a
hubertTCM <fuqunxing@gmail.com>
2014-01-14 15:41:35 +0800

    recommit missed files

---
+++ dataImporter/MedicalNotesProvider/provider_hhjfsl.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+import sys
+import os
+import codecs
+import re
+
+def append_ancestors_to_system_path(levels):
+    parent = os.path.dirname(__file__)
+    for i in range(levels):
+        sys.path.append(parent)
+        parent = os.path.abspath(os.path.join(parent, ".."))
+        
+append_ancestors_to_system_path(3)
+
+from dataImporter.Utils.Utility import *
+
+class hhjfsl:
+    pass

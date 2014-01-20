760953e8a009411900e3a0d60344513b991b270f
hubertTCM <fuqunxing@gmail.com>
2014-01-20 13:22:58 +0800

    add clause, initial version, without any content

---
+++ dataImporter/ClauseProvider/GoldenChamber.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class GoldenChamberProvider:
+    def __init__(self):
+        pass
+    
+    def get_all_clauses(self):
+        clauses = []
+        return clauses
+
+
+
+if __name__ == "__main__":
+    print "done"
---
+++ dataImporter/ClauseProvider/TreatiseOnFebrileDiseases.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+class FebribleDiseaseProvider:
+    def __init__(self):
+        pass
+    
+    def get_all_clauses(self):
+        clauses = []
+        return clauses
+
+
+
+if __name__ == "__main__":
+    print "done"

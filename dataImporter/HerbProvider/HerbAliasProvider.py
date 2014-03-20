05caeaf80a0dd08f3878bb077fba15705da23d67
hubertTCM <fuqunxing@gmail.com>
2014-03-20 13:50:25 +0800

    correct the herb alias provider, ensure no new line char is in the name of herb

--- dataImporter/HerbProvider/HerbAliasProvider.py
+++ dataImporter/HerbProvider/HerbAliasProvider.py
@@ -21,7 +21,7 @@ from dataImporter.Utils.Utility import *
 class HerbAliasProvider:
     def __get_alias__(self, content, standard_name):
         pattern = ur'[ \u3000、]+'
-        items = filter(lambda(x):len(x)>0 and standard_name != x, [Utility.remove_blank_space(item) for item in re.split(pattern, content)])
+        items = filter(lambda(x):len(x)>0 and standard_name != x, [Utility.remove_blank_space(item.strip()) for item in re.split(pattern, content)])
         return items
         
     def get_all_alias_pair(self):
@@ -31,8 +31,6 @@ class HerbAliasProvider:
         
         alias_file = codecs.open(alias_file_path, 'r', 'utf-8', 'ignore')
         
-        #党  参：为桔梗科植物党参、素花党参、川党参的根。处方别名：党参、台党、潞党参、西党、汉中党、文党、文元党、晶党、东党、辽党、中灵草、上党人参
-        #混淆品：关白附：为毛茛科植物黄花乌头的块根。处方别名：关白附、竹节白附
         for line in alias_file: 
             pattern = ur"(混淆品：)*([^（）：]+)：\W+处方别名：(\W+)"       
             m = re.compile(pattern).match(line)
@@ -46,7 +44,8 @@ class HerbAliasProvider:
         
     
 if __name__ == "__main__":
-#     lines = [ur"混淆品：关白附：为毛茛科植物黄花乌头的块根。处方别名：关白附、竹节白附",
+#     
+#    lines = [ur"混淆品：关白附：为毛茛科植物黄花乌头的块根。处方别名：关白附、竹节白附",
 #              ur"混混品：关白附：为毛茛科植物黄花乌头的块根。处方别名：关白附、竹节白附",
 #              ur"党  参：为桔梗科植物党参、素花党参、川党参的根。处方别名：党参、台党、潞党参、西党、汉中党、文党、文元党、晶党、东党、辽党、中灵草、上党人参"]
 #     for line in lines:
@@ -64,5 +63,4 @@ if __name__ == "__main__":
     
     p = HerbAliasProvider()
     for alias, standard_name in p.get_all_alias_pair():
-        print alias
-        print standard_name
+        print alias, ":", standard_name
